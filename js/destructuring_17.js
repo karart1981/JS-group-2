44791ee8c92bc66ff229c9410d60dd9e6068ffc6
art.karapetyan <karapart1981@gmail.com>
2022-07-01 00:31:22 +0400

    I finished homework 17 about destructuring

--- js/destructuring_17.js
+++ js/destructuring_17.js
@@ -63,6 +63,145 @@ const [sum1, sum2,sum3, sum4=5, sum5=87] = sumNum;
 console.log(sum5);
 
 // օր 10 / array
+const arrLoop = [
+	{name:"Artur"},
+	{name:"Karen"},
+	{name:"Arshavir"},
+	{name:"Manuk"}
+];
+const [a1,a2,a3,a4] = arrLoop;
+console.log(a1, a2, a3, a4);
+
+
+// օր 1 / object
+const userInfo_1 = {
+	name_1:"Mila",
+	lastName_1:"Minasyan",
+    age_1:25,
+	country_1:"Spain",
+};
+const {name_1,lastName_1,age_1,country_1} = userInfo_1;
+console.log(`My name is ${name_1},lastName is ${lastName_1}, I am ${age_1} year age.`);
+
+
+// օր 2 / object
+const userInfo_2 = {
+   name:"Arevik",
+   "last-name":"Karapetyan",
+   age:18,
+   country:"Armenia"
+};
+const {name, a =userInfo_2["last-name"], age, country} = userInfo_2;
+
+console.log(name, a);
+
+// օր 3 / object
+const userInfo_3 = {
+	name_3:"Lilit",
+	lastName_3:"Pogkosyan",
+	age_3:45,
+	country_3:"Italy",
+	firiends_1:[
+		{name_4:"Margar",lastName_4:"Yesayan",age_4:47,country_4:"Cyprus",friends_2:[]},
+		{name_5:"Petros",lastName_5:"Minasyan",age_5:41,country_5:"Great Britain",friends_3:[]}
+	]
+};
+const {name_4} = userInfo_3.firiends_1[0];
+console.log(`${name_4} is my friend.`);
+
+// օր 4 / object / 3 օրինակից
+const {name_5, lastName_5, age_5, country_5, friends_3} =userInfo_3.firiends_1[1];
+console.log(`Username is ${name_5} ${lastName_5}, he is ${age_5} year age,he lives in ${country_5}.`);
+
+
+// օր 5 / object 
+const famousBuildings ={
+  name_b1: "Tower of Pisa",
+  height_b1: "59 m",
+  architect_b1: "	Bonanno Pisano",
+  location_b1:"Pisa, Italy"
+};
+
+function printBuildInfo({name_b1,height_b1,architect_b1,location_b1}){
+	console.log(`${name_b1} is in ${location_b1},it's height is ${height_b1}, architect-${architect_b1}.`);
+}
+printBuildInfo(famousBuildings);
+
+
+// օր 6 / object
+const famousBuildings2 = {
+name_b2:"Tatev Monastery",
+location_b2: "Syunik Province, Armenia",
+style_b2: "armenian",
+groundbreaking_b2: "8th century"
+};
+
+const {name_b2, location_b2, style_b2, groundbreaking_b2} = famousBuildings2;
+
+if(style_b2 === "armenian"){
+	console.log(`${name_b2} is in ${location_b2}.`);
+} else{
+	console.log(style_b2);
+}
+
+// օր 7 / object
+const anyObj = {
+	name:"Lily",
+	hobby:["tennis", "reading book", "walking in the park"]
+};
+const {name, hobby} = anyObj;
+const [tennis, book, walk] = anyObj.hobby;
+console.log(`${name} has some hobby,she like play ${tennis}, ${book}, ${walk}.`);
+
+
+// օր 8 / object
+const famousBuildings3 ={
+ name_b3:"the Great Wall",
+ location_b3: "China",
+ built_3: "over 2000 years",
+};
+const{name_b3,location_b3,built_3} = famousBuildings3;
+
+console.log(`Being the longest man-made structure ever built, ${name_b3} is a global tourist attraction. Its great length and historic value define its uniqueness.It is a series of fortifications built by different emperors for ${built_3}, which were then joined together, resulting in ${name_b3}. The purpose of this wall was to protect the people of ${location_b3} from its enemies. It also served as a barrier for the Country to separate the Chinese people and their culture from the rest of the World.`);
+
+
+// օր 9 / object
+const options = {
+	title: "Car",
+	model: "BMW M5",
+	year: 2020
+  };
+  const {title, model, year} = options;
+  console.log(title); 
+  console.log(model); 
+  console.log(year); 
+
+ // օր 10 / object
+ const options_2 = {
+	title_2:"Menu",
+	size: {
+	  width: 100,
+	  height: 200
+	},
+	items: ["Cake", "Donut"],
+	extra: true
+  };
+  
+  const {title_2 ,size: {width,height},items: [item1, item2]} = options_2;
+  
+  console.log(title_2);  
+  console.log(width);  
+  console.log(height); 
+  console.log(item1);  
+  console.log(item2);  
+
+
+
+
+
+
+
+
 
 
 
--- js/filmapp.js
+++ js/filmapp.js
@@ -1,66 +1,73 @@
-"use strict"; 
-
-/* 
-1․ գլխավոր օբջեքթի մեջ ստեղծել showMyDB մեթոդ, որը կստուգի privat-ը, եթե լինի
-false ապա գլխավոր օբջեքթը չպետք ա կոնսոլ լինի, եթե true ապա պետք ա կոնսոլ լինի
-2․ գլխավոր օբջեքթի մեջ ստեղծել yourFavoriteGenres մեթոդ, որի մեջ յուզեռը պետք ա 
-երեք անգամ պատասխանի այց հարցին ՝ « Ձեր նախընտրելի ժանրը» ու կողքից գրվի հարցի 
-թիվը, այսինքն առաջին հարցը կլինի 1, երկրորդ հարցը կլինի 2, երրորդ հարցը կլինի 3, 
-ու էդ ամեն ինչը պետք ա գրեք genres անունով զանգվածի մեջ
-3․ պետք ա ներկայիս կոդը օպտիմիզացնեք ու ձեր ստեղծած մեթոդները դնեք ստուգման տակ,
-որպեսզի յուզեռը չկարողանաք ոչ քենսլ անի, ոչ դատարկ վալյու ուղարկի ոչ էլ 20ից շատ 
-տառ
-+4. numberOfFilms-ը պետք ա լինի ՄԻԱՅՆ ԹԻՎ, եթե թիվ չի while-ով էնքան անել, որ 
-յուզեռը թիվ գրի */
+"use strict";
 
 let numberOfFilms;
-if(numberOfFilms != "number" && numberOfFilms !== 0 && numberOfFilms < 0) {
-	numberOfFilms = +prompt("Քանի՞ ֆիլմ եք դիտել այսօր", "Խնդրում եմ նշեք թիվ");
-	if(numberOfFilms <=10){
-		alert("Դուք ազատ ժամանակ նայում եք ֆիլմեր");
-	}else if(numberOfFilms <=20){
-		alert("Դուք կինոյի սիրահար եք");
-	} else{
-		alert("Դուք կինոման եք");
+
+function start () {
+	numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
+
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
 	}
-} else{
-  numberOfFilms --;
 }
 
-let a = personalMovieDB.movies;
-let result = 0;
-if(a !== "" || a !== null || a < 20 || a !== undefined || a != "number") {
-	a = prompt("Նշեք վերջին ֆիլմը, որը դիտել եք", "Խնդրում եմ գրեք հայերեն" );
-}else{
-	result --;
-}
+start();
 
-
-let b;
-do {
-	b = +prompt("1-10 ինչքա՞ն կգնահատեք այդ ֆիլմը", "Խնդրում եմ նշեք թիվ");
-} while (b === 0 || isNaN(b));
-
-let c = personalMovieDB.genres;
-let res = 0;
-if(c === "" || c === null || c.length > 20 || c === undefined){
-	c = prompt("Ձեր նախընտրած ժանրը", "Խնդրում եմ գրեք հայերեն");
-} else{
-	c--;
-}
-
-	
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: true,
-	showMyDB(){
-		return  personalMovieDB.privat === true ? console.log("It's privat."): console.log("it is not privat.");
-	},
-	
+	privat: false
 };
-personalMovieDB.movies[a] = b;
-personalMovieDB.numberOfFilms = numberOfFilms;
-personalMovieDB.showMyDB();
+
+function rememberMyFilms () {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել");
+		const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք");
+	
+		if (!isNaN(b) && a != null && b != null && a.trim() != "" && b.trim() != "" && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log("DONE");
+		} else {
+			console.log("ERROR");
+			i--;
+		}
+	}
+}
+
+rememberMyFilms();
+
+function detectUserPersonalLevel() {
+	if (numberOfFilms < 10) {
+		console.log("Դուք նայել եք բավականին քիչ ֆիլմեր");
+	} else if (numberOfFilms < 30) {
+		console.log("Դուք ֆիլմի սիրահար եք");
+	} else if (numberOfFilms >= 30) {
+		console.log("Դուք կինոման եք !!!");
+	}
+}
+
+detectUserPersonalLevel();
+
+function showMyDB () {
+	if (personalMovieDB.privat === true) {
+		console.log(personalMovieDB);
+	} else {
+		console.log("Մեր տվյալների բազան փակ է");
+	}
+}
+
+showMyDB();
+
+function yourFavoriteGenres () {
+	for (let i = 0; i <= 2; i++) {
+		const genres = prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`);
+
+		if (genres != null && genres.trim() != "" && genres.length <= 20) {
+			personalMovieDB.genres[i] = genres;
+		} else {
+			i--;
+		}
+	}
+}
+yourFavoriteGenres();
