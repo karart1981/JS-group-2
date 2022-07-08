149950a5bc76362f3d1324e9d897c01b48207428
art.karapetyan <karapart1981@gmail.com>
2022-07-08 18:34:34 +0400

    I started to write tourismapp.js file

--- js/filmapp.js
+++ js/filmapp.js
@@ -1,22 +1,24 @@
 "use strict";
-
-let numberOfFilms;	
+// isVisibleMyDB
 const personalMovieDB = {
-	count: numberOfFilms,
+	count: 0,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: true,
-	start:function(){ 
-	   numberOfFilms =	parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", "Խնդրում եմ գրեք թվով"));
-		while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-			numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", "Խնդրում եմ գրեք թվով"));
+	start: function () {
+		personalMovieDB.count = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
+		
+		while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count) || personalMovieDB.count < -1) {
+			personalMovieDB.count = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
 		}
+	
+		return personalMovieDB.count;
 	},
-	rememberMyFilms:function() {
+	rememberMyFilms: function () {
 		for (let i = 0; i < 2; i++) {
-			const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել","Խնդրում եմ գրեք հայերեն");
-			const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք","Խնդրում եմ գրեք թվով");
+			const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");
+			const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք", "");
 		
 			if (!isNaN(b) && a != null && b != null && a.trim() != "" && b.trim() != "" && a.length < 50) {
 				personalMovieDB.movies[a] = b;
@@ -27,48 +29,73 @@ const personalMovieDB = {
 			}
 		}
 	},
-	detectUserPersonalLevel:function() {
-		if (numberOfFilms < 10) {
+	detectUserPersonalLevel: function () {
+		if (personalMovieDB.count < 10) {
 			console.log("Դուք նայել եք բավականին քիչ ֆիլմեր");
-		} else if (numberOfFilms < 30) {
+		} else if (personalMovieDB.count < 30) {
 			console.log("Դուք ֆիլմի սիրահար եք");
-		} else if (numberOfFilms >= 30) {
+		} else if (personalMovieDB.count >= 30) {
 			console.log("Դուք կինոման եք !!!");
+		} else {
+			console.log("Տեղի է ունեցել խնդիր, ըստ երևույթի դուք թիվ չեք նշել");
 		}
 	},
-	yourFavoriteGenres:function() {
-		for (let i = 0; i <= 2; i++) {
-			const genres = prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`,"Խնդրում եմ գրեք հայերեն");
+	yourFavoriteGenres: function () {
+		// for (let i = 0; i <= 2; i++) {
+		// 	const genres = prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`);
 	
-			if (genres != null && genres.trim() != "" && genres.length <= 20) {
-				personalMovieDB.genres[i] = genres;
+		// 	if (genres != null && genres.trim() != "" && genres.length <= 20) {
+		// 		personalMovieDB.genres[i] = genres;
+		// 	} else {
+		// 		console.log("Դուք թույլ եք տվել սխալ, խնդրում ենք հետևել կանոններին !");
+		// 		i--;
+		// 	}
+		// }
+
+		for (let i = 1; i < 2; i++) {
+			const genres = prompt("Թվարկեք ձեր նախընտրելի ժանրերը, եթե դրանք մեկից ավելի են, ապա խնդրում ենք առանձնացնել դրանք ստորակետերով", "");
+	
+			if (genres != null && genres.trim() != "") {
+				personalMovieDB.genres = genres.trim().toLowerCase().split(", ", 1000);
+				personalMovieDB.genres.sort();
 			} else {
+				console.log("Դուք թույլ եք տվել սխալ, խնդրում ենք հետևել կանոններին !");
 				i--;
 			}
 		}
-		personalMovieDB.genres.forEach (function (item , index)  { 
-			console.log(`Ձեր նախընտրելի ժանրը.ժանր ${index+1} ${item}`);}); 
+
+		// personalMovieDB.genres.forEach(function(genre, i){
+		// 	// if (i === 0) {
+		// 	// 	console.log(`Ձեր սիրելի ${i + 1}ին ժանրը։ ${genre}`);
+		// 	// } else {
+		// 	// 	console.log(`Ձեր սիրելի ${i + 1}րդ ժանրը։ ${genre}`);
+		// 	// }
+
+		// 	console.log(`Ձեր սիրելի ժանրը։ ${genre}`);
+		// });
 	},
-	showMyDB:function() {
-		if (personalMovieDB.privat === true) {
-			console.log(personalMovieDB);
+	isVisibleMyDB: function () {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
 		} else {
-			console.log("Մեր տվյալների բազան փակ է");
+			personalMovieDB.privat = true;
 		}
-		personalMovieDB.showMyDB();
 	},
-	isVisibleMyDB(){
-		if(personalMovieDB.privat === false){
-			console.log(personalMovieDB.privat = true );
-		} else{
-		console.log(personalMovieDB.privat = false);
-	    }
+	showMyDB: function (show) {
+		if (show) {
+			console.log(personalMovieDB);
+		}
+	},
+
+	// test function which can run our programm
+	run() {
+		personalMovieDB.start();
+		personalMovieDB.rememberMyFilms();
+		personalMovieDB.detectUserPersonalLevel();
+		personalMovieDB.yourFavoriteGenres();
+		personalMovieDB.isVisibleMyDB();
+		personalMovieDB.showMyDB(personalMovieDB.privat);
 	}
 };
-personalMovieDB.start();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.detectUserPersonalLevel();
-personalMovieDB.yourFavoriteGenres();
-personalMovieDB.isVisibleMyDB(personalMovieDB.showMyDB);
 
-
+// personalMovieDB.run();
